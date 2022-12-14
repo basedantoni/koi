f24c7e5a4c4a603ca97aac550b5ce77dc9bb3345
Anthony Mercado <antho@Anthonys-Mini.attlocal.net>
2022-12-13 23:30:09 -0600

    :lipstick: Finish v1 of UI

--- components/CaseStudy.js
+++ components/CaseStudy.js
@@ -2,19 +2,21 @@ import Image from "next/image"
 
 export default function CaseStudy ({ imageSrc, studyName, studyDescription }) {
   return (
-    <>
-      <Image
-        className="mt-16 mb-4 w-full"
-        src={imageSrc}
-        alt="picture of case study"
-        width={320}
-        height={243}
-      />
+    <div className="sm:flex sm:even:flex-row-reverse sm:my-[78px]">
+      <div className="sm:w-[650px] sm:h-[372px] sm:bg-orange-500">
+        <Image
+          className="mt-16 mb-4 w-full sm:w-[459px] sm:object-scale-down"
+          src={imageSrc}
+          alt="picture of case study"
+          width={320}
+          height={243}
+        />
+      </div>
 
-      <div className="mb-8 px-6">
+      <div className="mb-8 px-6 sm:w-[448px]">
         <h2 className="font-bold text-xl">{ studyName }</h2>
         <p className="mt-3">{ studyDescription }</p>
       </div>
-    </>
+    </div>
   )
 }
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,15 +2,15 @@ import CaseStudy from "../../components/CaseStudy";
 
 const caseStudies = [
   { name: 'Nike', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis elementum augue, non consectetur ipsum. Suspendisse fringilla finibus ultrices. Nam ultrices consequat nisl eget facilisis. Sed convallis augue eget ligula cursus, sit amet feugiat nibh iaculis. Vivamus lacinia efficitur metus vitae tincidunt. Nullam sed dapibus tortor. Sed pretium dui dolor, vel laoreet velit semper volutpat. Cras ultricies et ex fermentum efficitur. Mauris ut velit ornare, hendrerit neque eu, facilisis justo.', imageSrc: '/../public/images/nike.webp' },
-  { name: 'Nike', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis elementum augue, non consectetur ipsum. Suspendisse fringilla finibus ultrices. Nam ultrices consequat nisl eget facilisis. Sed convallis augue eget ligula cursus, sit amet feugiat nibh iaculis. Vivamus lacinia efficitur metus vitae tincidunt. Nullam sed dapibus tortor. Sed pretium dui dolor, vel laoreet velit semper volutpat. Cras ultricies et ex fermentum efficitur. Mauris ut velit ornare, hendrerit neque eu, facilisis justo.', imageSrc: '/../public/images/nike.webp' },
-  { name: 'Nike', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis elementum augue, non consectetur ipsum. Suspendisse fringilla finibus ultrices. Nam ultrices consequat nisl eget facilisis. Sed convallis augue eget ligula cursus, sit amet feugiat nibh iaculis. Vivamus lacinia efficitur metus vitae tincidunt. Nullam sed dapibus tortor. Sed pretium dui dolor, vel laoreet velit semper volutpat. Cras ultricies et ex fermentum efficitur. Mauris ut velit ornare, hendrerit neque eu, facilisis justo.', imageSrc: '/../public/images/nike.webp' },
+  { name: 'Homer', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis elementum augue, non consectetur ipsum. Suspendisse fringilla finibus ultrices. Nam ultrices consequat nisl eget facilisis. Sed convallis augue eget ligula cursus, sit amet feugiat nibh iaculis. Vivamus lacinia efficitur metus vitae tincidunt. Nullam sed dapibus tortor. Sed pretium dui dolor, vel laoreet velit semper volutpat. Cras ultricies et ex fermentum efficitur. Mauris ut velit ornare, hendrerit neque eu, facilisis justo.', imageSrc: '/../public/images/nike.webp' },
+  { name: 'Off-White', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis elementum augue, non consectetur ipsum. Suspendisse fringilla finibus ultrices. Nam ultrices consequat nisl eget facilisis. Sed convallis augue eget ligula cursus, sit amet feugiat nibh iaculis. Vivamus lacinia efficitur metus vitae tincidunt. Nullam sed dapibus tortor. Sed pretium dui dolor, vel laoreet velit semper volutpat. Cras ultricies et ex fermentum efficitur. Mauris ut velit ornare, hendrerit neque eu, facilisis justo.', imageSrc: '/../public/images/nike.webp' },
 ]
 
 export default function Projects () {
   return (
     <>
       <div className="mt-16 px-6">
-        <h1 className="font-bold text-4xl">Browse our latest and greatest</h1>
+        <h1 className="font-bold text-4xl sm:text-center">Browse our latest and greatest</h1>
       </div>
 
       {caseStudies.map(({ name, description, imageSrc }) => (
