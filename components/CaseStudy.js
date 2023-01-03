1857c207020204bb257896d0943ff100359719bf
Anthony Mercado <antho@Anthonys-Mini.attlocal.net>
2023-01-02 21:07:51 -0600

    :iphone: Finish mobile design

--- components/CaseStudy.js
+++ components/CaseStudy.js
@@ -2,14 +2,14 @@ import Image from "next/image"
 
 export default function CaseStudy ({ imageSrc, studyName, studyDescription }) {
   return (
-    <div className="sm:flex border-t-2 justify-between py-4">
+    <div className="sm:flex sm:border-t-2 justify-between py-4">
       <div className="mt-4 w-96">
         <h2 className="font-bold text-xl md:text-5xl">{ studyName }</h2>
         <p className="mt-4">{ studyDescription }</p>
       </div>
     
       <Image
-        className="w-full sm:max-w-sm sm:object-scale-down"
+        className="mt-4 sm:mt-0 w-full sm:max-w-sm sm:object-scale-down"
         src={imageSrc}
         alt="picture of case study"
         width={320}
