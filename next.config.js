bb92483ddb495f3ed44139fccfdf8b0644fe4e7e
Anthony Mercado <antho@Anthonys-Mini.attlocal.net>
2023-01-09 20:18:19 -0600

    :wrench: Add env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    env: process.env.ENV,
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import Layout from '../components/layout'
+import getConfig from "next/config";
 import '../styles/globals.css'
 
+const { publicRuntimeConfig } = getConfig();
+const environment = publicRuntimeConfig.env
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  if (environment === 'prod') {
+    return (
+      <Layout>Nothing to see here</Layout>
+    )
+  } else {
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
+  }
 }
 
 export default MyApp
